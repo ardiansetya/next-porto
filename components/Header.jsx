31b8d8dfc8ccd2a4d68c62919d5d04cfaadd5d0d
ardiansetya <ardiansetya129@gmail.com>
2025-01-20 20:56:50 +0700

    fix/header

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,9 +27,9 @@ const Header = () => {
       </p> */}
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
         <a
-          href="#contact"
+          href="https://github.com/ardiansetya"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">
-          Contact Me{" "}
+          My Github{" "}
           <Image src={assets.right_arrow_white} alt="profile" className="w-4" />{" "}
         </a>
         <a
